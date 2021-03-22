1573d03786637ae26ab007183c69d02e00c78e5c
johnjeang <johnjeang@gmail.com>
2021-03-22 09:17:50 -0500

    Clean PosterView

--- Quotes/Views/PosterView.swift
+++ Quotes/Views/PosterView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct PosterView: View {
+    
+    @ObservedObject var quoteModel = QuoteModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 20){
+                    ForEach(quoteModel.quotes){quote in
+                        NavigationLink(
+                            destination: AuthorView(quote: quote),
+                            label: {
+                                ZStack{
+                                    Image(quote.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .cornerRadius(30)
+                                    VStack(alignment: .leading, spacing: 10.0){
+                                        Text(quote.text[0])
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                        Text("- " + quote.author)
+                                }
+                                }
+                                .padding(.horizontal)
+                                .foregroundColor(Color.white)
+                                .frame(width: .none, height: 400, alignment: .center)
+                                .clipped()
+                        })
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Posters")
+        }
+
+        
     }
 }
 
